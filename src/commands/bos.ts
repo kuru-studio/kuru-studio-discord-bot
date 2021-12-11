bd08eafe81cc02579f85bb8efd84529a0b08f38f
Bos Eriko Reyes <bos.eriko@gmail.com>
2021-12-11 23:53:02 +0800

    Hotfix: Add a bos command

---
+++ src/commands/bos.ts
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'bos',
+  description: 'BosEriko!',
+  execute(message) {
+    message.channel.send('https://tenor.com/view/diluc-hello-tae-hello-tae-diluc-tae-hello-tae-im-coming-home-gif-23041768');
+  },
+};
