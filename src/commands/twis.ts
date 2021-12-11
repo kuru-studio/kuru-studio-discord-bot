5a412bac4f62aad8e236f6850d674875b0a2f104
Bos Eriko Reyes <bos.eriko@gmail.com>
2021-12-11 23:50:46 +0800

    Hotfix: Add a TwisWua command

---
+++ src/commands/twis.ts
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'twis',
+  description: 'TwisWua!',
+  execute(message) {
+    message.channel.send('https://tenor.com/view/animal-crossing-acnh-ass-animal-crossing-new-horizons-new-horizons-gif-21746420');
+  },
+};
