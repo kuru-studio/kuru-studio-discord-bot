e38867fd302a16b1688f725e6d2f0c441f3ff6d1
Bos Eriko Reyes <bos.eriko@gmail.com>
2021-12-12 03:07:11 +0800

    Hotfix: Add Yuunisu command

---
+++ src/commands/yuunisu.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'yuunisu',
+  description: 'Yuunisu!',
+  execute(message) {
+    message.channel.send('https://tenor.com/view/detective-pikachu-pikachu-pokemon-angry-frustrated-gif-21719143');
+    message.channel.send('Sino warfreak?!');
+  },
+};
